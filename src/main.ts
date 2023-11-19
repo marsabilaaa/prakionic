5c821b4ffe7038ac3c8e6bd2b1a0c1acc9b83bd3
marsabilaaa <marsabilaaa@gmail.com>
2023-11-20 03:22:11 +0700

    #

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { HttpClientModule } from '@angular/common/http';
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
 if (environment.production) {
-enableProdMode();
+  enableProdMode();
 }
-bootstrapApplication(AppComponent, {
-providers: [
-{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, },
-importProvidersFrom(IonicModule.forRoot({}),HttpClientModule),
-provideRouter(routes),
-],
-});
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
